7c0929f1e794352e8534edf18be440ef98f9f454
신국정 <orkaye7751@gmail.com>
2018-04-16 21:24:09 +0900

    퍼블리싱 및 라라벨 개발 부트스트랩

---
+++ publishing/web.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    //return view('welcome');
+    return view('main');
+});
+
+Route::get('/intro', function () {
+    return view('intro');
+});
+
+Route::get('/main', function () {
+    return view('main');
+});
